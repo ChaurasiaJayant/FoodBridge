27aa64ec56ad7eb8e59c710a1d2180945bc585ed
ChaurasiaJayant <chaurasiajayant087@gmail.com>
2026-08-13 03:38:22 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -165,9 +165,7 @@ const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(
-      `MongoDB connected successfully: ${connection.connection.host}`,
-    );
+    console.log(`MongoDB connected successfully`);
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
 
